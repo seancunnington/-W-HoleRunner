094c67f596b6b735927f9f89999ea8f02b8657db
seancunnington <cunnington.sean@gmail.com>
2020-02-17 16:14:51 -0800

    ScriptableObjects File Upload: These are for planet detail to make it easy for random generation. This will most likely be simplified.

---
+++ Assets/ScriptableObjects/Planets/PlanetLists/PlanetLists.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlanetLists", menuName = "new Planet List")]
+public class PlanetLists : ScriptableObject
+{
+     public string listName;
+
+     public Planets[] planets;
+     public Material[] materials;
+
+}
---
+++ Assets/ScriptableObjects/Planets/Planets.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu (fileName = "Planets", menuName = "new Planet")]
+public class Planets : ScriptableObject
+{
+     public string planetName;
+
+     public Mesh mesh;
+
+     [HideInInspector]
+     public Material material;
+
+}
